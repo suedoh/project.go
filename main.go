8b2bff0f4ed1acbd99978bae964624b667b18411
suedoh <suedoh36@proton.me>
2023-03-24 22:49:53 -0400

    adding logic to add a remote address

--- main.go
+++ main.go
@@ -26,11 +26,12 @@ func handleGit() {
         switch gitInput {
         case "y":
             cmdStruct := exec.Command("git", "init")    
-            out,err := cmdStruct.Output()
+            out, err := cmdStruct.Output()
             if err != nil {
                 log.Fatalln("Error:", err)
             }
             fmt.Println(string(out))
+            handlGitRemote()
             return
         case "n":
             fmt.Println("No git init")
@@ -41,4 +42,38 @@ func handleGit() {
             continue
         }
     }
+}
+
+func handlGitRemote() {
+    fmt.Println("Would you like to add a remote url? (y/n)")
+    
+    reader := bufio.NewReader(os.Stdin)
+    char, _, err := reader.ReadRune()
+    if err != nil {
+        log.Fatalln("Following error occurred:", err)
+    }
+
+    if string(char) != "y" {
+        fmt.Println("y not provided, not adding remote")
+        return
+    }
+
+    fmt.Println("Please provide the remote address: ")
+    reader = bufio.NewReader(os.Stdin)
+    remoteAddr, _, err := reader.ReadLine()
+    if err != nil {
+        log.Fatalln("Following error occurred:", err)
+    }
+
+    // add regex and string sanitizing
+    address := string(remoteAddr)
+    fmt.Println(address)
+
+    cmdStruct := exec.Command("git", "remote", "add", "origin", address)    
+    out, err := cmdStruct.Output()
+    if err != nil {
+        log.Fatalln("Error:", err)
+    }
+
+    fmt.Println(out)
 }
